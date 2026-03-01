1ea4bd82877341bac262dcf35fb72eefb0667772
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-01 19:31:16 +0530

    added admin  login controlller and generated access and refresh tokens

--- server/src/controllers/admin.js
+++ server/src/controllers/admin.js
@@ -3,6 +3,26 @@ import ApiResponse from "../utils/ApiResponse.js";
 import ApiError from "../utils/ApiErrors.js";
 import { Admin } from "../models/admin.model.js";
 
+const generateAccessAndRefreshTokens = async (adminId) => {
+  try {
+    const admin = await Admin.findById(adminId);
+
+    const accessToken = admin.generateAccessToken();
+    const refreshToken = admin.generateRefreshToken();
+
+    admin.accessToken = accessToken;
+    admin.refreshToken = refreshToken;
+
+    await admin.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (e) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating access and refresh Tokens"
+    );
+  }
+};
 export const registerAdmin = asyncHandler(async (req, res) => {
   const { email, password, fullname } = req.body;
 
@@ -32,4 +52,50 @@ export const registerAdmin = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(new ApiResponse(200, createdAdmin, "Admin Registered Successfully"));
+});
+
+export const loginAdmin = asyncHandler(async (req, res) => {
+  //take data from frontend user
+  const { email, password } = req.body;
+
+  //validation
+  if (!email) {
+    throw new ApiError(404, "email is required");
+  }
+
+  //if present check if admin is registered or not
+  const admin = await Admin.findOne({ email });
+
+  if (!admin) {
+    throw new ApiError(409, "Admin with this Email id is not registered");
+  }
+
+  //if registered then compare the password if it is correct or not
+  const isPassValid = await admin.isPasswordCorrect(password);
+
+  //if password is not valid throw an error
+  if (!isPassValid) {
+    throw new ApiError(404, "Password is not correct");
+  }
+
+  //if password is correct then generate access and refresh tokens
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    admin._id
+  );
+  console.log("acc token", accessToken);
+  console.log("refresh token=", refreshToken);
+
+  const loggedInAdmin = await Admin.findById(admin._id).select("-password");
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        loggedInAdmin,
+        accessToken,
+        refreshToken,
+      },
+      "Admin Logged In successfully"
+    )
+  );
 });
