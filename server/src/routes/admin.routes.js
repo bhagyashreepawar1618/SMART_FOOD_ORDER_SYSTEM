d54f96dfc3e80fbd2d472a580abf43d24735f343
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-01 19:31:30 +0530

    added admin login route

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,8 +1,9 @@
-import { registerAdmin } from "../controllers/admin.js";
+import { registerAdmin, loginAdmin } from "../controllers/admin.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.route("/register").post(registerAdmin);
+router.route("/login").post(loginAdmin);
 
 export default router;
