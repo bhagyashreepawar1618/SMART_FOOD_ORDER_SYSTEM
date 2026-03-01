966f96a09b3b0f1c14717acaf0f8fdd0d8317848
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-02 00:34:19 +0530

    added route to get menu

--- server/src/routes/students.routes.js
+++ server/src/routes/students.routes.js
@@ -1,7 +1,11 @@
-import { registerStudent, loginStudent } from "../controllers/students.js";
+import {
+  registerStudent,
+  loginStudent,
+  getTodaysMenu,
+} from "../controllers/students.js";
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(
@@ -13,6 +17,6 @@ router.route("/register").post(
   ]),
   registerStudent
 );
-
 router.route("/login").post(loginStudent);
+router.route("/getmenu").post(verifyJWT, getTodaysMenu);
 export default router;
