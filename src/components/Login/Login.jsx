457e2c801bf87b5be77a3a68ed542f2b449f7748
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-01 16:37:20 +0530

    student login component updated applied loading animation and stored token value in context

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,39 @@
-import React, { useState } from "react";
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const [name, setName] = useState("");
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleSubmit(e) {
+  //for loading state
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    console.log("Login Name:", name);
-    console.log("Password:", password);
-    // yaha baad me backend connect hoga
+    setLoading(true);
+
+    //backend call
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/v1/students/login",
+        {
+          username,
+          password,
+        },
+      );
+
+      localStorage.setItem("accessToken", response.data.data.accessToken);
+
+      setLoading(false);
+      setUsername("");
+      setPassword("");
+      alert("Student is Logged in successfully");
+      navigate("/dashboard", { replace: true });
+      console.log("response from Backend=", response);
+    } catch (e) {}
   }
 
   return (
@@ -33,8 +57,8 @@ function Login() {
             <input
               type="text"
               placeholder="Enter your name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               required
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-[#AB274F]"
             />
@@ -58,9 +82,40 @@ function Login() {
           {/* Button */}
           <button
             type="submit"
-            className="w-full py-2 rounded-lg bg-[#AB274F] text-white font-semibold hover:opacity-90 transition"
+            disabled={loading}
+            className="w-full py-3 rounded-xl 
+bg-[#AB274F] 
+hover:bg-[#8e1f42] 
+disabled:bg-[#c46a86] 
+text-white font-semibold tracking-wide 
+transition-all duration-300 
+shadow-md hover:shadow-[#AB274F]/40 
+hover:scale-[1.02] 
+flex items-center justify-center gap-2"
           >
-            Login
+            {loading && (
+              <svg
+                className="w-5 h-5 animate-spin"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                />
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v8H4z"
+                />
+              </svg>
+            )}
+            {loading ? "Logging in..." : "Login"}
           </button>
         </form>
 
