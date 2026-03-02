e7886bfb3a80e8549148addc4ac644c15fb6a50c
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-03 00:39:40 +0530

    added get all stats route

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -5,6 +5,7 @@ import {
   updateAdminProfile,
   updatePassword,
   getAllOrders,
+  getMenuStats,
 } from "../controllers/admin.js";
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/authAdmin.middleware.js";
@@ -17,5 +18,6 @@ router.route("/setmenu").post(setMenuData);
 router.route("/update-profile").post(verifyJWT, updateAdminProfile);
 router.route("/update-password").post(verifyJWT, updatePassword);
 router.route("/all-orders").get(verifyJWT, getAllOrders);
+router.route("/get-menu-stats/:menuId").get(verifyJWT, getMenuStats);
 
 export default router;
