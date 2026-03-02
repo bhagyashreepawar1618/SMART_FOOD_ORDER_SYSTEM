909045da2e656e5a464efd7d12ac68453289e9d5
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-02 18:56:35 +0530

    added update password route

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -3,6 +3,7 @@ import {
   loginAdmin,
   setMenuData,
   updateAdminProfile,
+  updatePassword,
 } from "../controllers/admin.js";
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/authAdmin.middleware.js";
@@ -13,5 +14,6 @@ router.route("/register").post(registerAdmin);
 router.route("/login").post(loginAdmin);
 router.route("/setmenu").post(setMenuData);
 router.route("/update-profile").post(verifyJWT, updateAdminProfile);
+router.route("/update-password").post(verifyJWT, updatePassword);
 
 export default router;
